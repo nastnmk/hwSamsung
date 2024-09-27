0207f58f292169a61f5c2c01cf938fa7a416751f
Nastya <naumkana22@gmail.com>
2024-09-27 21:26:20 +0300

    Initial commit

---
+++ src/com/samsung/while1.java
@@ -0,0 +1,16 @@
+package com.samsung;
+import java.util.Scanner;
+public class while1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 1;
+        while (true){
+            int a = scanner.nextInt();
+            if (a<0){
+                break;
+            }
+            sum+=1;
+        }
+        System.out.println(sum);
+    }
+}
